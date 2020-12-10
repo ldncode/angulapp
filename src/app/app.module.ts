4c5a2aaa36ec86cf5c7a66799151c646ba22816a
LDN <ledenevdn@gmail.com>
2020-12-10 17:33:19 +0700

    Add rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
